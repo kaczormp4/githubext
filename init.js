0b363855ab76a1dbf2afd09b999b9365527506ea
kaczormp4 <kaczkaoryginal@gmail.com>
2025-06-10 18:01:31 +0200

    test 2

--- init.js
+++ init.js
@@ -1,12 +1,13 @@
-import { createLeftPanel } from "./ui/leftPanel.js";
-import { createRightPanel, setupDiffToggle } from "./ui/rightPanel.js";
-import { injectStyles } from "./styles/main.css.js";
-import { logAction } from "./core/logger.js";
+// import { createLeftPanel } from "./ui/leftPanel.js";
+// import { createRightPanel, setupDiffToggle } from "./ui/rightPanel.js";
+// import { injectStyles } from "./styles/main.css.js";
+// import { logAction } from "./core/logger.js";
 
 (function () {
   "use strict";
-  injectStyles();
-  createLeftPanel();
-  createRightPanel();
-  setupDiffToggle(logAction);
+  console.log("xdd");
+  //   injectStyles();
+  //   createLeftPanel();
+  //   createRightPanel();
+  //   setupDiffToggle(logAction);
 })();
